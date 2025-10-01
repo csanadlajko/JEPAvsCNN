2da5a58a9c228b94dc608b249edd9a7bd8d70a74
csanaed <csanilajko@gmail.com>
2025-10-01 22:28:32 +0200

    Initialized hyperparameters and created model

--- src/JEPA/vit/vit.py
+++ src/JEPA/vit/vit.py
@@ -1,6 +1,19 @@
 import torch.nn as nn
 import torch
 
+## hyperparameters
+
+DEPTH = 6
+DROP_RATE = 0.1
+BATCH_SIZE = 128
+CHANNELS = 3
+EMBED_DIM = 256
+IMG_SIZE = 32
+PATCH_SIZE = 4
+MLP_DIM = 512
+NUM_HEADS = 8
+EPOCHS = 10
+
 class MLP(nn.Module):
     
     def __init__(self, in_features, hidden_features=None, out_features=None, act_layer=nn.GELU, drop=0.):
@@ -29,10 +42,15 @@ class PatchEmbed(nn.Module):
         self.patch_size = patch_size
         self.num_patches = (img_size // patch_size) ** 2
         self.proj = nn.Conv2d(in_chans, embed_dim, kernel_size=patch_size, stride=patch_size)
+        self.cls_token = nn.Parameter(torch.zeros(1, 1, embed_dim))
+        self.pos_embed = nn.Parameter(torch.zeros(1, self.num_patches + 1, embed_dim))
         
     def forward(self, x):
         B, C, H, W = x.shape # -> should be B N D
         x = self.proj(x).flatten(2).transpose(1, 2)
+        cls_tokens = self.cls_token.expand(B, -1, -1)
+        x = torch.cat((cls_tokens, x), dim=1)
+        x = x + self.pos_embed
         return x
     
 
@@ -61,14 +79,14 @@ class VisionTransformer(nn.Module):
             in_chans=in_chans,
             embed_dim=embed_dim
         )
-        self.encoder = nn.Sequential([
+        self.encoder = nn.Sequential(*[
             TransformerEncoder(
                 num_heads=num_heads,
                 embed_dim=embed_dim,
                 mlp_dim=mlp_dim,
                 drop=drop_rate
             )
-            for _ in range(6)
+            for _ in range(DEPTH)
         ])
         self.norm = nn.LayerNorm(embed_dim)
         
@@ -80,42 +98,14 @@ class VisionTransformer(nn.Module):
         x = self.norm(x)
         cls_token = x[:, 0]
         return cls_token # return self.head(cls_token) when classification
-        
-    
-class ViTInput(nn.Module):
-    
-    def __init__(self, patch_embed):
-        super().__init__()
-        self.patch_embed = patch_embed
-        num_patches = self.patch_embed.num_patches
-        self.cls_token = nn.Parameter(torch.zeros(1, 1, self.patch_embed.embed_dim))    
-        self.pos_embed = nn.Parameter(torch.zeros(1, num_patches + 1, self.patch_embed.embed_dim))
-        
-    def forward(self, x):
-        B = x.shape[0]
-        x = self.patch_embed(x)
-        cls_tokens = self.cls_token.expand(B, -1, -1)
-        x = torch.cat((cls_tokens, x), dim=1)
-        
-        x = x + self.pos_embed
-        return x
-    
-class InputTest:
-    
-    def __init__(self):
-        patch_embed = PatchEmbed(
-            img_size=4,
-            patch_size=2,
-            in_chans=1,
-            embed_dim=3
-        )
-        self.vit_input = ViTInput(patch_embed)
-        self.img = torch.arange(1, 17).float().reshape(1, 1, 4, 4) # numbers 1-16 in correct shape
-        
-    def __test__(self):
-        print("-----STARTING VIT TRANSFORM-----")
-        out = self.vit_input(self.img)
-        print("-----VIT TRANSFORM FINISHED-----")
-        
-t2 = InputTest()
-t2.__test__()
+
+model = VisionTransformer(
+    img_size=IMG_SIZE,
+    patch_size=PATCH_SIZE,
+    in_chans=CHANNELS,
+    embed_dim=EMBED_DIM,
+    num_heads=NUM_HEADS,
+    depth=DEPTH,
+    mlp_dim=MLP_DIM,
+    drop_rate=DROP_RATE
+)
